ecce38be849d5b65d08ad21cdc343fcef91beaa4
ClinMers <colin.myers013@gmail.com>
2024-08-07 16:17:07 -0500

    ClueGame Splash screen

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -14,6 +14,14 @@ public class ClueGame extends JPanel{
 	
 
 	public static void main(String[] args) {
+		
+		Board.createInstance();
+        Board board = Board.getInstance();
+        board.initialize();
+
+        
+        
+        
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Medieval Clue");
@@ -30,5 +38,14 @@ public class ClueGame extends JPanel{
 		frame.add(gameControl, BorderLayout.SOUTH);
 		
 		frame.setVisible(true);
+		
+		// Get the human player details the show splash screen with players character
+		Player humanPlayer = Board.getHuman(); 
+		showSplashScreen(humanPlayer);
 	}
+	
+	private static void showSplashScreen(Player humanPlayer) {
+        String message = "You are: The " + humanPlayer.getName() + "\nCan you find the solution\nbefore the Computer players?";
+        JOptionPane.showMessageDialog(null, message, "Welcome To Clue", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
