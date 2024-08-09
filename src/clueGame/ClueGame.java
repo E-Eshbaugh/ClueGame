1690899abf50919947a88c5fd7986adce63f989c
E-Eshbaugh <eeshbaugh@mines.edu>
2024-08-09 13:34:52 -0400

    added some more organization to ClueGame

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -18,6 +18,14 @@ public class ClueGame extends JPanel{
 	private Board board;
 	protected static HumanPlayer humanPlayer;
 	
+	
+	//=============================================================================================\\
+	//=======================------------------- SETUP METHODS -------------=======================\\
+	//=============================================================================================\\
+	
+	/*======================
+	 * Constructor
+	 ========================*/
 	private ClueGame() {
 		initializeBoard();
 		setupFrame();
@@ -26,10 +34,11 @@ public class ClueGame extends JPanel{
 		setupControlPanel();
 	}
 	
-	private void displayGame() {
-		frame.setVisible(true);
-	}
 	
+	
+	/*================================
+	 * Sets up the game frame
+	 ==============================*/
 	private void setupFrame() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Medieval Clue");
@@ -37,29 +46,55 @@ public class ClueGame extends JPanel{
 		frame.setLayout(new BorderLayout());
 	}
 	
+	
+	
+	/*=========================
+	 * Creates the CardsGUIPanel
+	 * on right side of game frame
+	 * 
+	 * calls CardsGUIPanel()
+	 =============================*/
 	private void setupCardsPanel() {
 		CardsGUIPanel cardsPanel = new CardsGUIPanel();
 		cardsPanel.setPreferredSize(new Dimension(200, 0));
 		frame.add(cardsPanel, BorderLayout.EAST);
 	}
 	
+	
+	
+	/*===================================
+	 * Sets up the panel that holds the 
+	 * game board
+	 * 
+	 * calls board.drawBoard()
+	 =====================================*/
 	private void setupGamePanel() {
 		JPanel gamePanel = board.drawBoard();
 		gamePanel.setPreferredSize(new Dimension(700,800));
 		frame.add(gamePanel, BorderLayout.CENTER);
 	}
 	
+	
+	
+	/*================================
+	 * sets up and creates the game 
+	 * control panel at the bottom of
+	 * the game frame
+	 * 
+	 * calls GameControlPanel()
+	 ====================================*/
 	private void setupControlPanel() {
 		GameControlPanel gameControl = new GameControlPanel();
 		gameControl.setPreferredSize(new Dimension(0,200));
 		frame.add(gameControl, BorderLayout.SOUTH);
 	}
 	
-	private static void showSplashFrame() { 
-		String message = "You are: The " + humanPlayer.getName() + "\nCan you find the solution\nbefore the Computer players?";
-        JOptionPane.showMessageDialog(null, message, "Welcome To Clue", JOptionPane.INFORMATION_MESSAGE);
-	}
 	
+	
+	/*=======================
+	 * initializes the board
+	 * instance for the game
+	 ========================*/
 	private void initializeBoard() {
 		Board.createInstance();
         board = Board.getInstance();
@@ -67,7 +102,31 @@ public class ClueGame extends JPanel{
         humanPlayer = Board.getHuman();
 	}
 	
-
+	//=============================================================================================\\
+	//=======================--------------- GAME CONTROL METHODS ---------========================\\
+	//=============================================================================================\\
+	
+	/*=========================
+	 * Display game frame
+	 =========================*/
+	private void displayGame() {
+		frame.setVisible(true);
+	}
+	
+	
+	
+	/*===========================
+	 * Displays splash frame
+	 ==============================*/
+	private static void showSplashFrame() { 
+		String message = "You are: The " + humanPlayer.getName() + "\nCan you find the solution\nbefore the Computer players?";
+        JOptionPane.showMessageDialog(null, message, "Welcome To Clue", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	//=============================================================================================\\
+	//==============================------------- MAIN --------------==============================\\
+	//=============================================================================================\\
+	
 	public static void main(String[] args) {
 		
 		ClueGame clueGame = new ClueGame();
