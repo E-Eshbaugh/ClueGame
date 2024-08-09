a3f7fd9bfa9732e4dc5d97d17b43d8449eade16f
E-Eshbaugh <eeshbaugh@mines.edu>
2024-08-09 12:39:10 -0400

    reworked ClueGame to have actual methods to make it easier for game control later

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -10,42 +10,67 @@ package clueGame;
 import java.awt.*;
 import javax.swing.*;
 
+//auto generated suppress to hide warning about no UID long return (?) 
+@SuppressWarnings("serial")
 public class ClueGame extends JPanel{
 	
-
-	public static void main(String[] args) {
-		
-		Board.createInstance();
-        Board board = Board.getInstance();
-        board.initialize();
-        
-		JFrame frame = new JFrame();
+	private JFrame frame = new JFrame();
+	private Board board;
+	
+	private ClueGame() {
+		initializeBoard();
+		setupFrame();
+		setupGamePanel();
+		setupCardsPanel();
+		setupControlPanel();
+	}
+	
+	private void displayGame() {
+		frame.setVisible(true);
+	}
+	
+	private void setupFrame() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Medieval Clue");
 		frame.setSize(900, 1000);
 		frame.setLayout(new BorderLayout());
-		
-		JPanel gamePanel = board.drawBoard();
-		gamePanel.setPreferredSize(new Dimension(700,800));
-		frame.add(gamePanel, BorderLayout.CENTER);
-		
+	}
+	
+	private void setupCardsPanel() {
 		CardsGUIPanel cardsPanel = new CardsGUIPanel();
 		cardsPanel.setPreferredSize(new Dimension(200, 0));
 		frame.add(cardsPanel, BorderLayout.EAST);
-		
+	}
+	
+	private void setupGamePanel() {
+		JPanel gamePanel = board.drawBoard();
+		gamePanel.setPreferredSize(new Dimension(700,800));
+		frame.add(gamePanel, BorderLayout.CENTER);
+	}
+	
+	private void setupControlPanel() {
 		GameControlPanel gameControl = new GameControlPanel();
 		gameControl.setPreferredSize(new Dimension(0,200));
 		frame.add(gameControl, BorderLayout.SOUTH);
-		
-		frame.setVisible(true);
-		
-		// Get the human player details the show splash screen with players character
-		Player humanPlayer = Board.getHuman(); 
-		showSplashScreen(humanPlayer);
 	}
 	
-	private static void showSplashScreen(Player humanPlayer) {
-        String message = "You are: The " + humanPlayer.getName() + "\nCan you find the solution\nbefore the Computer players?";
+	private static void showSplashFrame() {
+		Player humanPlayer = Board.getHuman(); 
+		String message = "You are: The " + humanPlayer.getName() + "\nCan you find the solution\nbefore the Computer players?";
         JOptionPane.showMessageDialog(null, message, "Welcome To Clue", JOptionPane.INFORMATION_MESSAGE);
-    }
+	}
+	
+	private void initializeBoard() {
+		Board.createInstance();
+        board = Board.getInstance();
+        board.initialize();
+	}
+	
+
+	public static void main(String[] args) {
+		
+		ClueGame clueGame = new ClueGame();
+		clueGame.displayGame();
+		showSplashFrame();
+	}
 }
